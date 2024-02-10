2f3914e39772f39cfc0960cfed4114c83cb2d4f0
Gabriel Hamel <gabriel.hamel.pro@gmail.com>
2024-02-10 17:54:25 +0100

    feat: Load discord token with dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
-import { Client, Events, VoiceState } from "discord.js";
+import { Client, Events, GatewayIntentBits, VoiceState } from "discord.js";
+import dotenv from "dotenv";
 import { DiscordUserState } from "./adapters/discord/userState";
 import { onUserJoinChannel } from "./core/events/onUserJoinChannel";
 
-const bot = new Client({
-  intents: 0,
-});
+void (async () => {
+  dotenv.config();
 
-bot.on(
-  Events.VoiceStateUpdate,
-  async (oldState: VoiceState, newState: VoiceState) => {
-    const oldUserState = new DiscordUserState(oldState);
-    const newUserState = new DiscordUserState(newState);
+  const discordClient = new Client({
+    intents: GatewayIntentBits.GuildVoiceStates,
+  });
 
-    await onUserJoinChannel(oldUserState, newUserState);
-  },
-);
+  discordClient.on(
+    Events.VoiceStateUpdate,
+    async (oldState: VoiceState, newState: VoiceState) => {
+      const oldUserState = new DiscordUserState(oldState);
+      const newUserState = new DiscordUserState(newState);
+
+      await onUserJoinChannel(oldUserState, newUserState);
+    },
+  );
+
+  await discordClient.login(process.env.DISCORD_BOT_TOKEN);
+})();
