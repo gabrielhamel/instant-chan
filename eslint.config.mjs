a1efd7f6b77ed630be441314bf5ac4fb213191f5
Gabriel Hamel <gabriel.hamel.pro@gmail.com>
2024-07-16 18:48:26 +0200

    Setup lint, format scripts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,11 +3,13 @@ import javascript from "@eslint/js";
 import typescript from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
 
+/**
+ * @see https://eslint.org/docs/latest/use/configure/configuration-files
+ * @type {import("@eslint/js")}
+ */
 export default [
   {
     files: ["**/*.{mjs,ts}"],
-  },
-  {
     languageOptions: {
       globals: globals.node,
     },
